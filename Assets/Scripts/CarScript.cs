298b72fe255078d0ebf8dae555a440c69ec55668
Kevin.V <1025532@student.sae.edu.au>
2025-07-16 10:39:55 +1000

    Adding new race elements

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -4,10 +4,18 @@ using Unity.Netcode;
 public class CarScript : NetworkBehaviour
 {
     public Rigidbody rigid;
-    public Vector3 centerOfMassOfCar = new Vector3(0f, -0.5f, 0f);
+    public Vector3 centerOfMassOfCar = new Vector3(0f, -1.0f, -0.8f);
     public WheelCollider wheel1, wheel2, Steeringwheel3, Steeringwheel4;
     private CarControls carcontrols;
     private bool isDrifting = false;
+    
+    //reset timer
+    private float upsideDownTimer = 0f;
+    private float resetDelay = 2f; // seconds before reset
+    
+    //Drifting Marks
+    public TrailRenderer[] tyreMarks;
+    
     [SerializeField] private GameObject cameraObject;
     
     private float verticalInput;
@@ -29,8 +37,13 @@ public class CarScript : NetworkBehaviour
     {
         if (!IsServer) return;
         ApplyMovement_Rpc();
-    }
 
+        if (IsServer)
+        {
+            CheckIfUpsideDown();
+        }
+        
+    }
     
     [Rpc(SendTo.ClientsAndHost, RequireOwnership = false)]
     private void ApplyMovement_Rpc()
@@ -46,7 +59,24 @@ public class CarScript : NetworkBehaviour
         Steeringwheel3.steerAngle = steer;
         Steeringwheel4.steerAngle = steer;
 
-        ApplyFriction(isDrifting); 
+        if (isDrifting == true)
+        {
+            ApplyGrip(wheel1, sideStiffness:0.2f, forwardStiffness:0.2f);
+            ApplyGrip(wheel2, sideStiffness:0.2f, forwardStiffness:0.2f);
+            //front wheels
+            ApplyGrip(Steeringwheel3, sideStiffness:1.2f, forwardStiffness:1.8f);
+            ApplyGrip(Steeringwheel4, sideStiffness:1.2f, forwardStiffness:1.8f);
+            startEmmiter();
+        }
+        else
+        {
+            ApplyGrip(wheel1, sideStiffness: 1.0f, forwardStiffness: 1.3f);
+            ApplyGrip(wheel2, sideStiffness: 1.0f, forwardStiffness: 1.3f);
+            //front wheels
+            ApplyGrip(Steeringwheel3, sideStiffness: 1.2f, forwardStiffness: 1.4f);
+            ApplyGrip(Steeringwheel4, sideStiffness: 1.2f, forwardStiffness: 1.4f);
+            stopEmmiter();
+        }
     }
 
     public void ReceiveInput(float vInput, float hInput, bool driftinput)
@@ -55,26 +85,64 @@ public class CarScript : NetworkBehaviour
         horizontalInput = hInput;
         isDrifting = driftinput; 
     }
-    private void ApplyFriction(bool drifting)
+    private void ApplyGrip(WheelCollider wheels, float sideStiffness, float forwardStiffness)
     {
-        WheelFrictionCurve rearFriction = Steeringwheel3.sidewaysFriction;
-      
+        WheelFrictionCurve sideFriction = Steeringwheel3.sidewaysFriction;
+        sideFriction.extremumValue = 2.0f;
+        sideFriction.asymptoteValue = 1.0f;
+        sideFriction.stiffness = sideStiffness;
+        wheels.sidewaysFriction = sideFriction;
+        
 
-        if (drifting)
+        WheelFrictionCurve forwardFriction = Steeringwheel3.forwardFriction;
+        forwardFriction.extremumValue = 2.0f;
+        forwardFriction.asymptoteValue = 1.0f;
+        forwardFriction.stiffness = forwardStiffness;
+        wheels.forwardFriction = forwardFriction;
+        
+    }
+
+    private void CheckIfUpsideDown()
+    {
+        if (Vector3.Dot(transform.up, Vector3.up) < 0.1f)
         {
-            rearFriction.extremumValue = 0.2f;
-            rearFriction.asymptoteValue = 0.1f;
-            rearFriction.stiffness = 0.4f;   
+            upsideDownTimer += Time.fixedDeltaTime;
+
+            if (upsideDownTimer >= resetDelay)
+            {
+                ResetCarUpright();
+                upsideDownTimer = 0f;
+            }
         }
         else
         {
-            rearFriction.extremumValue = 2.0f;
-            rearFriction.asymptoteValue = 1.0f;
-            rearFriction.stiffness = 1.0f;
+            upsideDownTimer = 0f; // reset if not upside down
         }
-        //fwd so keeping the front 2 gripped
-        wheel1.sidewaysFriction = rearFriction;
-        wheel2.sidewaysFriction = rearFriction;
+    }
+    private void ResetCarUpright()
+    {
+        Vector3 currentEuler = transform.eulerAngles;
+        transform.eulerAngles = new Vector3(0, currentEuler.y, 0);
         
+        transform.position += Vector3.up * 1.5f;
+        
+        rigid.linearVelocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    private void startEmmiter()
+    {
+        foreach (TrailRenderer T in tyreMarks)
+        {
+            T.emitting = true;
+        }
+    }
+
+    private void stopEmmiter()
+    {
+        foreach (TrailRenderer T in tyreMarks)
+        {
+            T.emitting = false;
+        }
     }
 }
